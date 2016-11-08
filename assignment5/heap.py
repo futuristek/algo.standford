e3c6c68ad4fbdfe6746f7091014da1d49f99aeba
futuristek <ginojay@gmail.com>
2016-11-08 15:36:08 -0500

    add update method

--- assignment5/heap.py
+++ assignment5/heap.py
@@ -66,8 +66,8 @@ class MinHeap(Heap):
 				self.a[i], self.a[p] = self.a[p], self.a[i]
 				self.bubble_up(p)
 
-	def insert(self, key):
-		self.a.append(key)
+	def insert(self, k):
+		self.a.append(k)
 		t = len(self.a) - 1
 		self.bubble_up(t)
 
@@ -95,78 +95,41 @@ class MinHeap(Heap):
 		self.bubble_down(0)
 		return tmp
 
-if __name__ == '__main__':
-	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-	print("array: ", str(b))
-	min_heap = MinHeap(b[:])
-	max_heap = MaxHeap(b[:])
-	min_heap.build_heap()
-	max_heap.build_heap()
-	print("min heap: ", str(min_heap.a))
-	print("max heap: ", str(max_heap.a))	
-	print("insert 0: ")
-	min_heap.insert(0)
-	print("min heap result: ", str(min_heap.a))
-	print("extract min: ", str(min_heap.extract()))
-	print("min heap result: ", str(min_heap.a))
+	# update the key of any index, bubble_up or bubble_down
+	def update(self, i, k):
+		if i < 0 or i >= len(self.a): return
+		self.a[i] = k		
+		l = Heap.left(self, i)
+		r = Heap.right(self, i)
+		p = Heap.parent(self, i)
+		if p > -1 and self.a[p] > k:
+			self.a[p], self.a[i] = self.a[i], self.a[p]
+			self.bubble_up(p)
+		else:
+			smallest = 0
+			if l < len(self.a) and self.a[l] < k:
+				smallest = k
+			if r < len(self.a) and self.a[r] < k:
+				smallest = r
+			if smallest != 0:
+				self.a[0], self.a[smallest] = self.a[smallest], self.a[0]
+				self.buble_down(smallest)
 
-# class Heap:
-	
-# 	def parent(self, i):
-# 		if i == 0:
-# 			return -1
-# 		else:
-# 			return int((i+1)/2) - 1
-
-# 	def left(self, i):
-# 		return 2*i + 1
-
-# 	def right(self, i):
-# 		return 2*i + 2
-
-# 	def max_heapify(self, i):
-# 		l = self.left(i)
-# 		r = self.right(i)
-# 		a = self.a
-# 		largest = i
-# 		if l < len(a) and a[l] > a[i]:
-# 			largest = l
-# 		if r < len(a) and a[r] > a[largest]:
-# 			largest = r
-# 		if largest != i:
-# 			a[i], a[largest] = a[largest], a[i]
-# 			self.max_heapify(largest)
-
-# 	def min_heapify(self, i):
-# 		l = self.left(i)
-# 		r = self.right(i)
-# 		a = self.a
-# 		smallest = i
-# 		if l < len(a) and a[l] < a[i]:
-# 			smallest = l
-# 		if r < len(a) and a[r] < a[smallest]:
-# 			smallest = r
-# 		if smallest != i:
-# 			a[i], a[smallest] = a[smallest], a[i]
-# 			self.min_heapify(smallest)
-
-# 	def build_max_heap(self, a):
-# 		self.a = a
-# 		s = len(a)
-# 		for i in reversed(xrange(int(s/2))):
-# 			self.max_heapify(i)
-	
-# 	def build_min_heap(self, a):
-# 		self.a = a
-# 		s = len(a)
-# 		for i in reversed(xrange(int(s/2))):
-# 			self.min_heapify(i)
-
-# 	def test_heap(self):
-# 		a = [5, 1, 2, 3, 4, 6, 7]
-# 		print "Test:"
-# 		print "array: " + str(a)
-# 		self.build_max_heap(a[:])
-# 		print "max heap: " + str(self.a)
-# 		self.build_min_heap(a[:])
-# 		print "min heap: " + str(self.a)
+# if __name__ == '__main__':
+# 	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# 	print("array: ", str(b))
+# 	min_heap = MinHeap(b[:])
+# 	max_heap = MaxHeap(b[:])
+# 	min_heap.build_heap()
+# 	max_heap.build_heap()
+# 	print("min heap: ", str(min_heap.a))
+# 	print("max heap: ", str(max_heap.a))	
+# 	print("insert 0: ")
+# 	min_heap.insert(0)
+# 	print("result: ", str(min_heap.a))
+# 	print("extract min: ", str(min_heap.extract()))
+# 	print("result: ", str(min_heap.a))
+# 	print("update heap: ")
+# 	min_heap = MinHeap(b[:])
+# 	min_heap.update(5, 6.5)
+# 	print("result: ", str(min_heap.a))
